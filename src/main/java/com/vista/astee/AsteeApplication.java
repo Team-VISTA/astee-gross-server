ecbfdb81fbfe23ca1e077d9b562b5f00b2a17353
sukyeongs <sukyeongseo@ewhain.net>
2022-12-06 13:15:10 +0900

    INIT
    initial commit

---
+++ src/main/java/com/vista/astee/AsteeApplication.java
@@ -0,0 +1,13 @@
+package com.vista.astee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsteeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsteeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vista/astee/AsteeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vista.astee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsteeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
